ef562de7235be883a38f2bc3d151f212b1cd3c32
jrjood <youssifbebo@gmail.com>
2025-05-19 23:36:44 +0300

    Update

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,14 @@ export default defineConfig({
   base: '/IGNIT-Crew', // Set repo name here
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
   },
 });
